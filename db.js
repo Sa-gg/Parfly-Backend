a1e0479f6cec25fb31c63e4fed69676329a9df89
Patrick Sagum <sagum.patrick.r@gmail.com>
2025-05-04 11:17:13 +0800

    dasdasd

--- db.js
+++ db.js
@@ -2,23 +2,25 @@ import pg from 'pg';
 import env from 'dotenv';
 
 env.config();
-const db = new pg.Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT
 
-})
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+  ssl: {
+    rejectUnauthorized: false // Important for Render’s self-signed certs
+  }
+});
 
 db.connect();
 
 db.on('error', (err) => {
-    console.error('Unexpected error on idle client', err)
-    process.exit(-1)
-})
-
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
 
 export const query = (text, params) => {
-    return db.query(text, params)
-}   
+  return db.query(text, params);
+};
